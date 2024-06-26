4603bd47de8756b52ea0559441c246d5b6c5d4d0
SantiAV25 <agredosantiago11@gmail.com>
2024-06-26 11:28:42 -0500

    creation of the springboot project

---
+++ src/main/java/com/neotropic_sas_backendTest/com/neotropicSASBackendTest/NeotropicSasBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.neotropic_sas_backendTest.com.neotropicSASBackendTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeotropicSasBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeotropicSasBackendTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neotropic_sas_backendTest/com/neotropicSASBackendTest/NeotropicSasBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neotropic_sas_backendTest.com.neotropicSASBackendTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeotropicSasBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
